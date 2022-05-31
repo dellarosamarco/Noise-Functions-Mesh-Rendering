bda59a0396bb5b1bdaad14f534d7af4d07843ac5
dellarosamarco <dellarosamarco00@gmail.com>
2022-05-31 23:51:44 +0200

    octaves

--- Scripts/Perlin.cs
+++ Scripts/Perlin.cs
@@ -8,22 +8,69 @@ public class Perlin : MonoBehaviour
         float amplitude = noiseData.amplitude;
         float frequency = perlinNoiseData.frequency;
         float scale = perlinNoiseData.scale;
+        int octaves = perlinNoiseData.octaves;
+        float persistance = perlinNoiseData.persistance;
+        float lacunarity = perlinNoiseData.lacunarity;
+        Vector2 offset = noiseData.offset;
 
         int xSize = noiseData.mapSize.x;
         int ySize = noiseData.mapSize.y;
 
+        float lowestNoiseHeight = 0f;
+        float highestNoiseHeight = 0f;
+
         float[,] perlinNoise = new float[xSize, ySize];
+
+        Vector2[] octavesOffsets = new Vector2[octaves];
+        System.Random rd = new System.Random();
+        for (int i = 0; i < octavesOffsets.Length; i++)
+        {
+            float xOffset = Random.Range(-100000,100000) + offset.x;
+            float yOffset = Random.Range(-100000, 100000) + offset.y;
+            octavesOffsets[i] = new Vector2(xOffset, yOffset);
+        }
 
         for (int x = 0; x < xSize; x++)
         {
             for (int y = 0; y < ySize; y++)
             {
-                float noiseHeight = Mathf.PerlinNoise(x * frequency / scale,y * frequency / scale) * amplitude;
+                float octaveFrequency = frequency;
+                float octaveAmplitude = amplitude;
+                float noiseHeight = 0f;
+
+                for (int i = 0; i < octaves; i++)
+                {
+                    float xOctave = x / scale * octaveFrequency + octavesOffsets[i].x;
+                    float yOctave = y / scale * octaveFrequency + octavesOffsets[i].y;
+
+                    float perlinValue = Mathf.PerlinNoise(xOctave, yOctave) * 2 - 1;
+                    noiseHeight += perlinValue * octaveAmplitude;
+
+                    octaveAmplitude *= persistance;
+                    octaveFrequency *= lacunarity;
+                }
 
                 if (noiseCurve != null)
                     perlinNoise[x, y] = noiseCurve.Evaluate(noiseHeight);
                 else
                     perlinNoise[x, y] = noiseHeight;
+
+                if (noiseHeight > highestNoiseHeight)
+                {
+                    highestNoiseHeight = noiseHeight;
+                }
+                else if (noiseHeight < lowestNoiseHeight)
+                {
+                    lowestNoiseHeight = noiseHeight;
+                }
+            }
+        }
+
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                perlinNoise[x, y] = Mathf.InverseLerp(lowestNoiseHeight, highestNoiseHeight, perlinNoise[x, y]) * amplitude;
             }
         }
 
--- Scripts/structs/PerlinNoiseData.cs
+++ Scripts/structs/PerlinNoiseData.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 public struct PerlinNoiseData
 {
     public float frequency;
-
     [Range(1,999)]
     public float scale;
+    public int octaves;
+    [Range(0f,1f)]
+    public float persistance;
+    public float lacunarity;
 }
